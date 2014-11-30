c2b75eb4fb9827f55c5645438fb8b0d258c39c5c
Sarah <sarah.c.koehler@gmail.com>
2014-11-30 22:48:12 +0100

    rechnernetze: block 4: einrueckung korrigiert (vim ftw)

--- Rechnernetze/block04/udpClientHash.c
+++ Rechnernetze/block04/udpClientHash.c
@@ -5,7 +5,7 @@
 # Die Weitergabe, Veröffentlichung etc. auch in Teilen ist nicht gestattet #
 #                                                                          #
 ############################################################################
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,37 +23,36 @@
 #define MAX_BUFFER_LENGTH 100
 
 int packData(char *buffer, char *order, uint16_t key, uint16_t value) {
-    strcpy(buffer, order);
-    // snprintf(buffer, sizeof order, "%s", order);
+	strcpy(buffer, order);
 
-    uint16_t tmp = htons(key);
-    buffer[4] = tmp >> 8;
-    buffer[5] = tmp;
+	uint16_t tmp = htons(key);
+	buffer[4] = tmp >> 8;
+	buffer[5] = tmp;
 
-    tmp = htons(value);
-    buffer[6] = tmp >> 8;
-    buffer[7] = tmp;
+	tmp = htons(value);
+	buffer[6] = tmp >> 8;
+	buffer[7] = tmp;
 
-    return 0;
+	return 0;
 }
 
 void unpack(unsigned char *buffer, uint16_t *key, uint16_t *value) {
-    uint16_t tmp;
-    
-    tmp = (buffer[4] << 8) | buffer[5];
-    *key = ntohs(tmp);
+	uint16_t tmp;
 
-    tmp = (buffer[6] << 8) | buffer[7];
-    *value = ntohs(tmp);
+	tmp = (buffer[4] << 8) | buffer[5];
+	*key = ntohs(tmp);
+
+	tmp = (buffer[6] << 8) | buffer[7];
+	*value = ntohs(tmp);
 }
 
 int getServer(uint16_t key, struct sockaddr *their_addr, int their_size, int sockfd) {
-	char order[8];
+	unsigned char order[8];
 	packData(order, "GET", key, 0);
 
 	sendto(sockfd, order, sizeof(order), 0, their_addr, their_size);
 
-	char buffer[8];
+	unsigned char buffer[8];
 
 	recv(sockfd, buffer, sizeof buffer, 0);
 
@@ -72,12 +71,12 @@ int getServer(uint16_t key, struct sockaddr *their_addr, int their_size, int soc
 }
 
 int setServer(uint16_t key, uint16_t value, struct sockaddr *their_addr, int their_size, int sockfd) {
-	char order[8];
+	unsigned char order[8];
 	packData(order, "SET", key, value);
 
 	sendto(sockfd, order, sizeof(order), 0, their_addr, their_size);
 
-	char buffer[8];
+	unsigned char buffer[8];
 
 	recv(sockfd, buffer, sizeof buffer, 0);
 
@@ -113,77 +112,77 @@ int delServer(uint16_t key, struct sockaddr *their_addr, int their_size, int soc
 
 int main(int argc, char *argv[])
 {
-    int sockfd;
-    struct sockaddr_in their_addr; // connector's address information
-    struct hostent *he;
-    int serverPort;
-  	uint16_t i;
-  	unsigned int n = 4;
-  	uint16_t keys[n];
-    uint16_t value = 0;
+	int sockfd;
+	struct sockaddr_in their_addr; // connector's address information
+	struct hostent *he;
+	int serverPort;
+	uint16_t i;
+	unsigned int n = 4;
+	uint16_t keys[n];
+	uint16_t value = 0;
 
-    printf("TCP client example\n\n");
+	printf("TCP client example\n\n");
 
-    if (argc != 3) {
-        fprintf(stderr,"Usage: tcpClient serverName serverPort\n");
-        exit(1);
-    }
+	if (argc != 3) {
+		fprintf(stderr,"Usage: tcpClient serverName serverPort\n");
+		exit(1);
+	}
 
-    serverPort = atoi(argv[2]);
+	serverPort = atoi(argv[2]);
 
-    //Resolv hostname to IP Address
-    if ((he=gethostbyname(argv[1])) == NULL) {  // get the host info
-        herror("gethostbyname");
-        exit(1);
-    }
-    /* ******************************************************************
-    Create Socket
-    ******************************************************************* */
+	// Resolv hostname to IP Address
+	if ((he=gethostbyname(argv[1])) == NULL) {  // get the host info
+		herror("gethostbyname");
+		exit(1);
+	}
+	/* ******************************************************************
+	   Create Socket
+	 ******************************************************************* */
 
-    sockfd = socket(PF_INET, SOCK_DGRAM, 0);
-    
-    //setup transport address
-    their_addr.sin_family = AF_INET;
-    their_addr.sin_port = htons(serverPort);
-    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-    memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
+	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 
-    
-    /* ******************************************************************
-    Send Data
-    ******************************************************************* */
-
-  	srand(time(NULL));
-
-  	for (i = 0; i < n; i++) {
-  		keys[i] = rand();
-  	}
-
-  	for (i = 0; i < n; i++) {
-  		value = rand();
-      printf("key: %i \n", keys[i]);
-      printf("value: %i \n", value);
-      setServer(keys[i], value, (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
-  	}
-
-  	for (i = 0; i < n; i++) {
-  		getServer(keys[i], (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
-  	}
-
-  	for (i = 0; i < n; i++) {
-  		delServer(keys[i], (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
-  	}
+	//setup transport address
+	their_addr.sin_family = AF_INET;
+	their_addr.sin_port = htons(serverPort);
+	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+	memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
 
 
-  	for (i = 0; i < n; i++) {
-  		getServer(keys[i], (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
-  	}
+	/* ******************************************************************
+	   Send Data
+	 ******************************************************************* */
 
-    
-    /* ******************************************************************
-     Close socket
-    ******************************************************************* */
-    close(sockfd);
+	srand(time(NULL));
 
-    return 0;
+	for (i = 0; i < n; i++) {
+		keys[i] = rand();
+	}
+
+	for (i = 0; i < n; i++) {
+		value = rand();
+		printf("key: %i \n", keys[i]);
+		printf("value: %i \n", value);
+		setServer(keys[i], value, (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
+	}
+
+	for (i = 0; i < n; i++) {
+		getServer(keys[i], (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
+	}
+
+	for (i = 0; i < n; i++) {
+		delServer(keys[i], (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
+	}
+
+
+	for (i = 0; i < n; i++) {
+		getServer(keys[i], (struct sockaddr *) &their_addr, sizeof their_addr , sockfd);
+	}
+
+
+	/* ******************************************************************
+	   Close socket
+	 ******************************************************************* */
+	close(sockfd);
+
+	return 0;
 }
